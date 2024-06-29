d12a95b1949c210548db03b0b6eae5aaefce0f97
WONIE <wonie@WONIEui-MacBookPro.local>
2024-06-29 09:16:40 +0900

    Feat | Design | Refactor | Rename | Test [ StorList ]
    - Rename [ SearchMenu -> StoreList ] for visualization
    - Test [ Add Test data ]
    - Refactor [ code ]
    - Design [ Card css ]
    - Feat :
    	- 검색 시 찾는 데이터가 맞으면 List를 보여줌
    	- 찾는 데이터가 있다면 데이터에 맞게 갯수 표시
    	- 동별/ 층별 선택이 끝나면 해당 선택에 맞게 보여줌
    - MenuOpen Link  변경

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,10 +1,15 @@
 /* eslint-disable */
 
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Form,
+} from 'react-router-dom';
 
 import Blog from './BlogPage/BlogPage';
-import SearchMenu from './SearchMenu/SearchMenu.js';
 import Park from './ParkPage/ParkPage.js';
 import QrPage from './QRpage/QrPage.js';
 import FindSpot from './FindSpot/FindSpot.js';
@@ -12,6 +17,7 @@ import FloorMenu from './FloorMenu/FloorMenu.js';
 import MainComponent from './FloorSpecific/MainComponent.js';
 import Main from './MainMenu/Main.js';
 import BlogPage from './BlogPage/Blog';
+import StoreList from './StoreList/StoreList.js';
 
 function App() {
   return (
@@ -22,8 +28,7 @@ function App() {
           <Route path="/main" element={<Main />} />
           <Route path="/store0" element={<Blog />} />
           <Route path="/storeinfo/:id" element={<BlogPage />} />
-
-          <Route path="/menu" element={<SearchMenu />} />
+          <Route path="/storelist" element={<StoreList />} />
           <Route path="/parkinfo" element={<Park />} />
           <Route path="/floorspecific" element={<MainComponent />} />
           <Route path="/qrpage" element={<QrPage />} />
