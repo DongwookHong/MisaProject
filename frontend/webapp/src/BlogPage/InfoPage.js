e3f5bd96e28e7741bfffceb0d9dad3ed53c0513c
KichanLee <lkc0847@naver.com>
2024-07-23 12:24:18 +0900

    FETCH 블로그페이지 내 지도 PIN 추가하기

--- frontend/webapp/src/BlogPage/InfoPage.js
+++ frontend/webapp/src/BlogPage/InfoPage.js
@@ -1,5 +1,84 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import { LuShare, LuChevronDown, LuChevronUp, LuMapPin } from 'react-icons/lu';
+import React, { useState, useMemo, useEffect, useRef } from "react";
+import { LuShare, LuChevronDown, LuChevronUp } from "react-icons/lu";
+import qrLocpin from "../asset/tool/locpin.png";
+
+const getManualBoundingRectFromPath = (pathElement) => {
+  const pathLength = pathElement.getTotalLength();
+  const points = [];
+  for (let i = 0; i < pathLength; i += pathLength / 10) {
+    const point = pathElement.getPointAtLength(i);
+    points.push(point);
+  }
+  const xs = points.map((p) => p.x);
+  const ys = points.map((p) => p.y);
+  const xMin = Math.min(...xs);
+  const xMax = Math.max(...xs);
+  const yMin = Math.min(...ys);
+  const yMax = Math.max(...ys);
+  return {
+    x: xMin,
+    y: yMin,
+    width: xMax - xMin,
+    height: yMax - yMin,
+  };
+};
+
+const drawLocpin = (svgDoc, ctx, blockId) => {
+  const img = new Image();
+  img.src = qrLocpin;
+
+  const width = 60;
+  const height = 60;
+
+  img.onload = () => {
+    if (!blockId) {
+      console.log("BlockId is missing");
+      return;
+    }
+
+    console.log("Drawing locpin for blockId:", blockId);
+
+    const targetElement = svgDoc.getElementById(blockId);
+
+    if (!targetElement) {
+      console.log(`Element not found for blockId:`, blockId);
+      return;
+    }
+
+    let cx, cy;
+
+    if (targetElement.tagName.toLowerCase() === "path") {
+      const rect = getManualBoundingRectFromPath(targetElement);
+      cx = rect.x + rect.width / 2 - width / 2;
+      cy = rect.y + rect.height / 2 - height / 2;
+    } else {
+      const x = parseFloat(targetElement.getAttribute("x")) || 0;
+      const y = parseFloat(targetElement.getAttribute("y")) || 0;
+      const elementWidth = parseFloat(targetElement.getAttribute("width"));
+      const elementHeight = parseFloat(targetElement.getAttribute("height"));
+
+      if (elementWidth === 0 || elementHeight === 0) {
+        console.log(`Element has invalid dimensions:`, targetElement);
+        return;
+      }
+
+      cx = x + elementWidth / 2 - width / 2;
+      cy = y + elementHeight / 2 - height / 2;
+    }
+
+    const offsetY = height / 2;
+    cy -= offsetY;
+
+    ctx.drawImage(img, cx, cy, width, height);
+
+    console.log(`Drawing locpin for element:`, targetElement);
+    console.log(`Locpin position: (${cx}, ${cy}), size: ${width}x${height}`);
+  };
+
+  img.onerror = () => {
+    console.error("Failed to load the image:", qrLocpin);
+  };
+};
 
 function InfoPage({
   store_name,
@@ -17,41 +96,59 @@ function InfoPage({
 }) {
   const [isExpanded, setIsExpanded] = useState(false);
   const [isLocationExpanded, setIsLocationExpanded] = useState(false);
-  const [svgContent, setSvgContent] = useState(null);
+  const canvasRef = useRef(null);
 
   useEffect(() => {
     const fetchSvgContent = async () => {
-      if (floor_image) {
+      if (floor_image && isLocationExpanded) {
         try {
           const response = await fetch(floor_image);
           const svgText = await response.text();
-          setSvgContent(svgText);
+          const parser = new DOMParser();
+          const svgDoc = parser.parseFromString(svgText, "image/svg+xml");
+          const svgElement = svgDoc.documentElement;
+
+          const canvas = canvasRef.current;
+          const ctx = canvas.getContext("2d");
+
+          const viewBox = svgElement.getAttribute("viewBox").split(" ");
+          canvas.width = parseFloat(viewBox[2]);
+          canvas.height = parseFloat(viewBox[3]);
+
+          const img = new Image();
+          img.onload = () => {
+            ctx.drawImage(img, 0, 0);
+
+            // Draw pin for the current store
+            drawLocpin(svgDoc, ctx, block_id);
+          };
+          img.src = "data:image/svg+xml;base64," + btoa(svgText);
         } catch (error) {
-          console.error('Error fetching SVG content:', error);
+          console.error("Error fetching SVG content:", error);
         }
       }
     };
 
     fetchSvgContent();
-  }, [floor_image]);
+  }, [floor_image, isLocationExpanded, block_id]);
 
   const getFloorDisplay = (floorNum) => {
     const num = Number(floorNum);
-    return num === 0 ? 'B1F' : `${num}F`;
+    return num === 0 ? "B1F" : `${num}F`;
   };
 
   const formatTime = (time) => {
-    return time === '00:00' ? '24:00' : time;
+    return time === "00:00" ? "24:00" : time;
   };
 
-  const daysOrder = ['일', '월', '화', '수', '목', '금', '토'];
-  
+  const daysOrder = ["일", "월", "화", "수", "목", "금", "토"];
+
   const sortedStoreHours = useMemo(() => {
     const today = new Date().getDay();
     return [...storeHours].sort((a, b) => {
       const aIndex = daysOrder.indexOf(a.dayOfWeek);
       const bIndex = daysOrder.indexOf(b.dayOfWeek);
-      return (aIndex - today + 7) % 7 - (bIndex - today + 7) % 7;
+      return ((aIndex - today + 7) % 7) - ((bIndex - today + 7) % 7);
     });
   }, [storeHours]);
 
@@ -60,7 +157,12 @@ function InfoPage({
     const todayKorean = daysOrder[today];
 
     return sortedStoreHours.map((day, index) => (
-      <p key={day.dayOfWeek} className={`business-day-info ${day.dayOfWeek === todayKorean ? 'today' : ''}`}>
+      <p
+        key={day.dayOfWeek}
+        className={`business-day-info ${
+          day.dayOfWeek === todayKorean ? "today" : ""
+        }`}
+      >
         <span className="day-label">{day.dayOfWeek}</span>
         {day.isOpen ? (
           <span className="time-info">
@@ -76,29 +178,32 @@ function InfoPage({
   const getCurrentBusinessStatus = () => {
     const now = new Date();
     const todayKorean = daysOrder[now.getDay()];
-    const todayHours = storeHours.find(day => day.dayOfWeek === todayKorean);
-    
-    if (!todayHours || !todayHours.isOpen) return '영업 종료';
-    
+    const todayHours = storeHours.find((day) => day.dayOfWeek === todayKorean);
+
+    if (!todayHours || !todayHours.isOpen) return "영업 종료";
+
     const currentTime = now.getHours() * 60 + now.getMinutes();
-    const openTime = parseInt(todayHours.openTime.split(':')[0]) * 60 + parseInt(todayHours.openTime.split(':')[1]);
-    const closeTime = parseInt(todayHours.closeTime.split(':')[0]) * 60 + parseInt(todayHours.closeTime.split(':')[1]);
-    
+    const openTime =
+      parseInt(todayHours.openTime.split(":")[0]) * 60 +
+      parseInt(todayHours.openTime.split(":")[1]);
+    const closeTime =
+      parseInt(todayHours.closeTime.split(":")[0]) * 60 +
+      parseInt(todayHours.closeTime.split(":")[1]);
+
     if (closeTime < openTime) {
-      // 자정을 넘어가는 경우
-      if (currentTime >= openTime || currentTime < closeTime) return '영업 중';
+      if (currentTime >= openTime || currentTime < closeTime) return "영업 중";
     } else {
-      if (currentTime >= openTime && currentTime < closeTime) return '영업 중';
+      if (currentTime >= openTime && currentTime < closeTime) return "영업 중";
     }
-    
-    return '영업 종료';
+
+    return "영업 종료";
   };
 
   const businessStatus = getCurrentBusinessStatus();
 
   const getModifiedBuildingDong = (dong) => {
-    if (dong === 'A') return '12BL';
-    if (dong === 'B') return '11BL';
+    if (dong === "A") return "12BL";
+    if (dong === "B") return "11BL";
     return dong;
   };
 
@@ -111,7 +216,8 @@ function InfoPage({
           </div>
           <div className="title-subsection">
             <h6 className="info_floor">
-              {building_name} {getModifiedBuildingDong(building_dong)} {getFloorDisplay(floor_number)}
+              {building_name} {getModifiedBuildingDong(building_dong)}{" "}
+              {getFloorDisplay(floor_number)}
             </h6>
             <div className="share-button" onClick={handleShare}>
               <LuShare />
@@ -121,15 +227,16 @@ function InfoPage({
         <hr className="light-line-full" />
         <div className="info-section">
           <div className="business-hours-container">
-            <div className="business-hours-header" onClick={() => setIsExpanded(!isExpanded)}>
+            <div
+              className="business-hours-header"
+              onClick={() => setIsExpanded(!isExpanded)}
+            >
               <span className="label">영업시간</span>
               <span className="business-status">{businessStatus}</span>
               {isExpanded ? <LuChevronUp /> : <LuChevronDown />}
             </div>
             {isExpanded && (
-              <div className="business-hours">
-                {renderBusinessHours()}
-              </div>
+              <div className="business-hours">{renderBusinessHours()}</div>
             )}
           </div>
           <p className="business-info">
@@ -140,34 +247,38 @@ function InfoPage({
             </a>
           </p>
           <p className="business-info">
-            <a href={home_page_path || insta_path} className="homepage-link" target="_blank" rel="noopener noreferrer">
+            <a
+              href={home_page_path || insta_path}
+              className="homepage-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               홈페이지 바로가기
             </a>
           </p>
           <hr className="light-line-full" />
         </div>
         <div className="location-info-container">
-          <div className="location-info-header" onClick={() => setIsLocationExpanded(!isLocationExpanded)}>
+          <div
+            className="location-info-header"
+            onClick={() => setIsLocationExpanded(!isLocationExpanded)}
+          >
             <span className="location-label">위치 정보</span>
             <span className="location-toggle">
-              {isLocationExpanded ? <LuChevronUp size={24} /> : <LuChevronDown size={24} />}
+              {isExpanded ? <LuChevronUp /> : <LuChevronDown />}
             </span>
           </div>
           {isLocationExpanded && (
             <div className="location-info">
-              {svgContent && (
-                <div 
-                  dangerouslySetInnerHTML={{ __html: svgContent }} 
-                  className="floor-image"
-                  style={{ width: '100%', marginBottom: '10px' }}
-                />
-              )}
-        
+              <canvas
+                ref={canvasRef}
+                style={{ width: "100%", marginBottom: "10px" }}
+              />
             </div>
           )}
         </div>
         <hr className="light-line-full" />
-        <div className="store-describe" style={{ whiteSpace: 'pre-line' }}>
+        <div className="store-describe" style={{ whiteSpace: "pre-line" }}>
           {store_info}
         </div>
         <hr className="light-line-full" />
--- frontend/webapp/src/FloorMenu/BuildingSelector.js
+++ frontend/webapp/src/FloorMenu/BuildingSelector.js
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import { AppContext } from './AppContext';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import '../style/FloorMenu/FloorMenu.css';
-import mapImage from '../asset/tool/mapimage.png';
+import React, { useContext } from "react";
+import { AppContext } from "./AppContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../style/FloorMenu/FloorMenu.css";
+import mapImage from "../asset/tool/mapimage.png";
 
 function BuildingSelector() {
   const { selectedBuilding, setSelectedBuilding, setFloorData } =
@@ -13,7 +13,7 @@ function BuildingSelector() {
   const buildingMap = {
     "힐스테이트 12BL": "힐스테이트 A동",
     "힐스테이트 11BL": "힐스테이트 B동",
-    "롯데캐슬": "롯데캐슬",
+    롯데캐슬: "롯데캐슬",
   };
 
   const handleBuildingSelect = async (displayName) => {
@@ -21,21 +21,23 @@ function BuildingSelector() {
     setSelectedBuilding(internalName);
 
     let buildingName, buildingDong;
-    if (internalName === '롯데캐슬') {
-      buildingName = '롯데캐슬';
-      buildingDong = 'C';
+    if (internalName === "롯데캐슬") {
+      buildingName = "롯데캐슬";
+      buildingDong = "C";
     } else {
-      [buildingName, buildingDong] = internalName.split(' ');
-      buildingDong = buildingDong.replace('동', '');
+      [buildingName, buildingDong] = internalName.split(" ");
+      buildingDong = buildingDong.replace("동", "");
     }
 
     try {
       // const response = await axios.get(`https://api.misarodeo.com/api/building/${buildingName}/${buildingDong}`);
-      const response = await axios.get(`/api/building/${buildingName}/${buildingDong}`);
+      const response = await axios.get(
+        `/api/building/${buildingName}/${buildingDong}`
+      );
       const parsedData = response.data.map((item) => JSON.parse(item));
       setFloorData(parsedData);
     } catch (error) {
-      console.error('Error fetching building data:', error);
+      console.error("Error fetching building data:", error);
     }
 
     navigate(`/floormenu?building=${internalName}&floor=1`);
@@ -44,21 +46,25 @@ function BuildingSelector() {
   const handleFloorSpecificClick = () => {
     if (selectedBuilding) {
       let buildingName, buildingDong;
-      if (selectedBuilding === '롯데캐슬') {
-        buildingName = '롯데캐슬';
-        buildingDong = 'C';
+      if (selectedBuilding === "롯데캐슬") {
+        buildingName = "롯데캐슬";
+        buildingDong = "C";
       } else {
-        [buildingName, buildingDong] = selectedBuilding.split(' ');
-        buildingDong = buildingDong.replace('동', '');
+        [buildingName, buildingDong] = selectedBuilding.split(" ");
+        buildingDong = buildingDong.replace("동", "");
       }
       navigate(`/${buildingName}/${buildingDong}`);
     } else {
-      console.log('건물을 선택해주세요.');
+      console.log("건물을 선택해주세요.");
     }
   };
 
   const getDisplayName = (internalName) => {
-    return Object.entries(buildingMap).find(([key, value]) => value === internalName)?.[0] || internalName;
+    return (
+      Object.entries(buildingMap).find(
+        ([key, value]) => value === internalName
+      )?.[0] || internalName
+    );
   };
 
   return (
@@ -68,10 +74,11 @@ function BuildingSelector() {
           key={displayName}
           className={`button ${
             selectedBuilding === buildingMap[displayName]
-              ? 'button-selected'
-              : 'button-unselected'
+              ? "button-selected"
+              : "button-unselected"
           }`}
-          onClick={() => handleBuildingSelect(displayName)}>
+          onClick={() => handleBuildingSelect(displayName)}
+        >
           {displayName}
         </button>
       ))}
@@ -81,7 +88,7 @@ function BuildingSelector() {
           alt="Icon"
           width="20"
           height="20"
-          style={{ padding: '5px' }}
+          style={{ padding: "5px" }}
         />
         층별안내
       </button>
