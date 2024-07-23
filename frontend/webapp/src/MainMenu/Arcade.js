51481d243c77b2201fb330d54f3ce2dc86d37b19
WONIE <wonie@WONIEui-MacBookPro.local>
2024-07-23 19:16:46 +0900

    Design
    [ Qrpage ] : dropdown | Locsearch 수정
    현재위치 부분 수정

--- frontend/webapp/src/MainMenu/Arcade.js
+++ frontend/webapp/src/MainMenu/Arcade.js
@@ -10,29 +10,48 @@ import hillstateA from '../asset/logo/hillA.png';
 import hillstateB from '../asset/logo/hillB.png';
 import lotteCastle from '../asset/logo/hLotte.png';
 
+import build from './build_001.png';
+import build_02 from './build_02.png';
+
+import mainTest from './newimage.jpg';
+
 import hillstateImg from '../asset/logo/Ar1.png'; // 실제 이미지 경로로 교체
 import lotteCastleImg from '../asset/logo/Ar2.png';
 function Arcade() {
   return (
-    <div className="arcade-container">
-      <div className="arcade-item" style={{ backgroundImage: `url(${hillstateImg})` }}>
+    <div
+      className="arcade-container"
+      // style={{ backgroundImage: `url(${build})` }}
+    >
+      <div
+        className="arcade-item"
+        // style={{ backgroundImage: `url(${hillstateImg})` }}
+        style={{ backgroundImage: `url(${hillstateImg})` }}>
         <div className="overlay">
           <div className="text">힐스테이트</div>
           <div className="arcade-fragment">
-            <Link to="/floormenu?building=힐스테이트 A동&floor=1" className="arcade-direction">
+            <Link
+              to="/floormenu?building=힐스테이트 A동&floor=1"
+              className="arcade-direction">
               12BL 바로가기 &gt;
             </Link>
-            <Link to="/floormenu?building=힐스테이트 B동&floor=1" className="arcade-direction">
+            <Link
+              to="/floormenu?building=힐스테이트 B동&floor=1"
+              className="arcade-direction">
               11BL 바로가기 &gt;
             </Link>
           </div>
         </div>
       </div>
-      <div className="arcade-item" style={{ backgroundImage: `url(${lotteCastleImg})` }}>
+      <div
+        className="arcade-item"
+        style={{ backgroundImage: `url(${lotteCastleImg})` }}>
         <div className="overlay">
           <div className="text">롯데 캐슬</div>
           <div className="arcade-fragment">
-            <Link to="/floormenu?building=롯데캐슬&floor=1" className="arcade-direction">
+            <Link
+              to="/floormenu?building=롯데캐슬&floor=1"
+              className="arcade-direction">
               바로가기 &gt;
             </Link>
             <Link to="/" className="arcade-direction">
