cde7dc90ad12d3ada02684c8cd7fc9825ee9c5b3
황제 <ahkiler@hwangje-ui-MacBookPro.local>
2024-08-16 16:51:22 +0900

    REFACTOR: JPA Repo Function return Optional<T>

--- backend/src/main/java/org/example/misa/repository/MemberRepository.java
+++ backend/src/main/java/org/example/misa/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByUsername(String username);
+    Optional<Member> findByUsername(String username);
 }
--- backend/src/main/java/org/example/misa/repository/StoreMemberRepository.java
+++ backend/src/main/java/org/example/misa/repository/StoreMemberRepository.java
@@ -4,7 +4,9 @@ import org.example.misa.domain.StoreMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StoreMemberRepository extends JpaRepository<StoreMember, Long> {
-    StoreMember findByStoreName(String storeName);
+    Optional<StoreMember> findByStoreName(String storeName);
 }
